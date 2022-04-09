7da19adcfdfe13bfc49c65afa64a91d597d579a4
Crowdbotics <team@crowdbotics.com>
2022-04-09 04:26:53 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/leaf_lyft_34663/MainActivity.java
@@ -0,0 +1,15 @@
+package com.leaf_lyft_34663;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "leaf_lyft_34663";
+  }
+}
